989393b9e39131f9b1afcd6c2d8a69fa5ac91d63
Idsosin <117338834+Idsosin@users.noreply.github.com>
2023-08-14 16:35:59 +0300

    Registration form
    - craete new file main/forms.py for RegisterUserForm
    -add new list item in base_index.html for registration on header
    - create registration.html
    - add new url and new view for registration

--- NODA_web/main/urls.py
+++ NODA_web/main/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import *
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('notes/', views.NotesPreview, name="notes")
+    path('notes/', views.NotesPreview, name="notes"),
+    path('register/', RegisterUser.as_view(), name='register'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- NODA_web/main/views.py
+++ NODA_web/main/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views.generic import CreateView
 from .models import Notes
+from django.urls import reverse_lazy
+from .forms import RegisterUserForm
+
+
+
+
 
 
 def NotesPreview(request):
@@ -10,3 +16,18 @@ def NotesPreview(request):
 
 def index(request):
     return render(request, 'main/index.html')
+
+
+# def registration(request):
+#     context = {}
+#     context['form'] = RegisterUserForm()
+#     return render(request, 'main/registration.html', context)
+
+class RegisterUser(CreateView):
+    form_class = RegisterUserForm
+    template_name = 'main/registration.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return dict(list(context.items()))
