d4d97d6d2f14ba19e147e0d49d13e3209528c432
Ivan Sosin <idsosin@gmail.com>
2023-08-16 14:55:54 +0300

    Fixed a bug
    - add new function (clean_email) on class RegisterUserForm in forms.py
    @Idsosin

--- NODA_web/main/forms.py
+++ NODA_web/main/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
+from django.core.exceptions import ValidationError
 
 class RegisterUserForm(UserCreationForm):
     username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
@@ -12,3 +12,23 @@ class RegisterUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+
+    def clean_email(self):
+        """Reject usernames that differ only in case."""
+        email = self.cleaned_data.get("email")
+        if (
+            email
+            and self._meta.model.objects.filter(email__iexact=email).exists()
+        ):
+            self._update_errors(
+                ValidationError(
+                    {
+                        "email": self.instance.unique_error_message(
+                            self._meta.model, ["email"]
+                        )
+                    }
+                )
+            )
+        else:
+            return email
